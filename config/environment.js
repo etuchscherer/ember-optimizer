7ba93906cf304cc1fb385a4e972897947e87ffb2
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2020-09-08 12:59:23 -0700

    Updating rootURL

--- config/environment.js
+++ config/environment.js
@@ -44,8 +44,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/ember-optimizer';
-    ENV.locationType = 'hash';
     // here you can enable a production-specific feature
   }
 
