b21152ac7187454b5ce53efe1a9fe1ab2282f090
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2020-09-14 14:39:22 -0700

    removing the autodatalayer - cuz it doens't put 'text' back in

--- app/initializers/component-override.js
+++ app/initializers/component-override.js
@@ -1,11 +1,11 @@
 import Component from '@ember/component';
-import { throttle } from '@ember/runloop';
+// import { throttle } from '@ember/runloop';
 
 export function initialize(/* application */) {
   Component.reopen({
     didRender() {
       console.log('pushing optimize.activate');
-      window.dataLayer.push({'event': 'optimize.activate'});
+      // window.dataLayer.push({'event': 'optimize.activate'});
       return this._super(...arguments);
     }
   })
